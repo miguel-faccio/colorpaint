e4d4178ed5c6867895b601ab4874b93924d8d356
MiguelFaccio <mifacciobossi@gmail.com>
2024-05-22 15:35:01 -0300

    alpha 1.0.2

--- repository/database.js
+++ repository/database.js
@@ -14,6 +14,10 @@ class Database {
 
     async verifyLogin(email, password) {
         try {
+            if (email === undefined || password === undefined) {
+                throw new Error("Email e password não podem ser undefined.");
+            }
+
             console.log('Verificando login no banco de dados');
             const sql = 'SELECT * FROM usuarios WHERE email = ? AND password = ?';
             const [rows, fields] = await this.#connection.execute(sql, [email, password]);
@@ -27,6 +31,10 @@ class Database {
 
     async registerUser(username, email, password) {
         try {
+            if (username === undefined || email === undefined || password === undefined) {
+                throw new Error("Username, email e password não podem ser undefined.");
+            }
+
             const sql = 'INSERT INTO usuarios (username, email, password) VALUES (?, ?, ?)';
             const [result] = await this.#connection.execute(sql, [username, email, password]);
             return result;
@@ -35,7 +43,38 @@ class Database {
             throw error;
         }
     }
+    async getAllUsers() {
+        try {
+            const sql = 'SELECT * FROM usuarios';
+            const [rows, fields] = await this.#connection.execute(sql);
+            return rows;
+        } catch (error) {
+            console.error('Erro ao recuperar usuários:', error);
+            throw error;
+        }
+    }
 
+    async getAllDrawings() {
+        try {
+            const sql = 'SELECT * FROM desenhos';
+            const [rows, fields] = await this.#connection.execute(sql);
+            return rows;
+        } catch (error) {
+            console.error('Erro ao recuperar desenhos:', error);
+            throw error;
+        }
+    }
+
+    async getUserById(id) {
+        try {
+            const sql = 'SELECT * FROM usuarios WHERE user_id = ?';
+            const [rows, fields] = await this.#connection.execute(sql, [id]);
+            return rows[0];
+        } catch (error) {
+            console.error('Erro ao buscar usuário pelo ID:', error);
+            throw error;
+        }
+    }
     async getUserDrawings(userId) {
         try {
             const sql = 'SELECT * FROM desenhos WHERE usuarios_user_id = ?';
@@ -46,6 +85,44 @@ class Database {
             throw error;
         }
     }
+
+    async updateUser(id, username, email, password) {
+        try {
+            const sql = 'UPDATE usuarios SET username = ?, email = ?, password = ? WHERE id = ?';
+            const [result] = await this.#connection.execute(sql, [username, email, password, id]);
+            return result;
+        } catch (error) {
+            console.error('Erro ao atualizar usuário:', error);
+            throw error;
+        }
+    }
+
+    async deleteUser(userId) {
+        try {
+            if (userId === undefined) {
+                throw new Error("User ID não pode ser undefined.");
+            }
+    
+            const sql = 'DELETE FROM usuarios WHERE user_id = ?'; // Ajuste aqui para usar user_id
+            const [result] = await this.#connection.execute(sql, [userId]);
+            return result;
+        } catch (error) {
+            console.error('Erro ao excluir usuário:', error);
+            throw error;
+        }
+    }
+
+
+    async deleteDrawing(id) {
+        try {
+            const sql = 'DELETE FROM desenhos WHERE id = ?';
+            const [result] = await this.#connection.execute(sql, [id]);
+            return result;
+        } catch (error) {
+            console.error('Erro ao excluir desenho:', error);
+            throw error;
+        }
+    }
 }
 
-module.exports = Database;
+module.exports = Database;            
--- startup.js
+++ startup.js
@@ -6,26 +6,28 @@ const Database = require('./repository/database');
 const consign = require("consign");
 
 const app = express();
+const db = new Database();
 
 // Configuração do EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'mvc/views'));
-app.set('views', path.join(__dirname, 'mvc/views/ctrldev')); // Caminho para o diretório de views
+app.set('views', path.join(__dirname, 'mvc', 'views'));
 
 // Configuração da sessão
 app.use(session({
-    secret: 'secret-key', // Você deve usar uma chave secreta segura
+    secret: 'secret-key', // Use uma chave secreta segura
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Deve ser true se estiver usando HTTPS
+    cookie: { secure: false } // Defina como true se estiver usando HTTPS
 }));
 
-const db = new Database();
-
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './mvc/views/ctrldev')));
-app.use(express.static(path.join(__dirname, './mvc/views/public')));
 
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'mvc/views/public')));
+app.use(express.static(path.join(__dirname, 'mvc/views/ctrldev')));
+
+// Rotas Públicas
 app.get("/", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.sendFile(path.resolve('mvc/views/public/colorpaint/index.html'));
@@ -36,24 +38,89 @@ app.get("/cadastro", (req, res) => {
     res.sendFile(path.resolve('mvc/views/public/colorpaint/cadastrar.html'));
 });
 
-app.get("/admin", (req, res) => {
+// Rota de Login
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    // Validação de entrada
+    if (!email || !password) {
+        console.log('Email e password são obrigatórios');
+        return res.status(400).redirect('/');
+    }
+
+    try {
+        console.log(`Tentativa de login com email: ${email}`);
+        const result = await db.verifyLogin(email, password);
+        console.log('Resultado da verificação de login:', result);
+
+        if (result.length > 0) {
+            req.session.user = {
+                id: result[0].id,
+                username: result[0].username,
+                email: result[0].email
+            };
+            console.log('Usuário logado:', req.session.user);
+            return res.redirect('/home');
+        } else {
+            console.log('Nenhum usuário encontrado com essas credenciais.');
+            return res.redirect('/');
+        }
+    } catch (error) {
+        console.error('Erro ao verificar o login:', error);
+        return res.redirect('/error');
+    }
+});
+
+// Rota de Registro
+app.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    // Validação de entrada
+    if (!username || !email || !password) {
+        console.log('Todos os campos são obrigatórios');
+        return res.status(400).redirect('/cadastro');
+    }
+
+    try {
+        const result = await db.registerUser(username, email, password);
+        if (result.affectedRows > 0) {
+            return res.redirect('/');
+        } else {
+            return res.redirect('/error');
+        }
+    } catch (error) {
+        console.error('Erro ao cadastrar usuário:', error);
+        return res.redirect('/error');
+    }
+});
+
+// Rotas Administrativas
+app.get("/admin", async (req, res) => {
     if (!req.session.user) {
         return res.redirect('/');
     }
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.render("crtldev/painel", { username: req.session.user.username }); // Passando o username para a view
+    try {
+        const usuarios = await db.getAllUsers();
+        const desenhos = await db.getAllDrawings();
+        res.render("ctrldev/painel", { username: req.session.user.username, usuarios, desenhos });
+    } catch (error) {
+        console.error('Erro ao carregar painel administrativo:', error);
+        res.redirect('/error');
+    }
 });
 
 app.get("/adm", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.sendFile(path.resolve('mvc/views/ctrldev/login.html'));
 });
+
 app.get("/cadadm", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.sendFile(path.resolve('mvc/views/ctrldev/casdastrar.html'));
+    res.sendFile(path.resolve('mvc/views/ctrldev/cadastrar.html'));
 });
 
-app.post('/loginAdm', async (req, res) => {
+app.post('/loginadm', async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
   
@@ -79,81 +146,102 @@ app.post('/loginAdm', async (req, res) => {
     }
 });
 
-app.post('/login', async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-  
-    try {
-        console.log(`Tentativa de login com email: ${email}`);
-        const result = await db.verifyLogin(email, password);
-        console.log('Resultado da verificação de login:', result);
-        if (result.length > 0) {
-            req.session.user = {
-                id: result[0].id,
-                username: result[0].username,
-                email: result[0].email
-            };
-            console.log('Usuário logado:', req.session.user);
-            return res.redirect('/home');
-        } else {
-            console.log('Nenhum usuário encontrado com essas credenciais.');
-            return res.redirect('/');
-        }
-    } catch (error) {
-        console.error('Erro ao verificar o login:', error);
-        return res.redirect('/error');
-    }
-});
-
-app.post('/register', async (req, res) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-  
-    try {
-        const result = await db.registerUser(username, email, password);
-        if (result.affectedRows > 0) {
-            res.redirect('/');
-        } else {
-            res.redirect('/error');
-        }
-    } catch (error) {
-        console.error('Erro ao cadastrar usuário:', error);
-        res.redirect('/error');
-    }
-});
-
 app.get("/home", (req, res) => {
     if (!req.session.user) {
         return res.redirect('/');
     }
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.render("public/colorpaint/canva", { username: req.session.user.username }); // Passando o username para a view
+    res.render("public/colorpaint/canva", { username: req.session.user.username });
 });
 
-app.get('/gallery', function(req, res) {
+app.get('/gallery', (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.render('public/colorpaint/gallery', { username: req.session.user.username });
 });
 
-
-
-app.get('/error', function(req, res) {
+app.get('/error', (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.render('ctrldev/error.ejs');
+    res.render('ctrldev/error');
 });
 
 app.post('/logout', (req, res) => {
     req.session.destroy((err) => {
         if (err) {
-            return console.log(err);
+            console.log(err);
+            return res.redirect('/error');
         }
         res.redirect('/');
     });
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Rota para exibir a página de edição de usuário
+app.get('/edit-user/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const usuario = await db.getUserById(id);
+        res.render('ctrldev/editarUser', { usuario });
+    } catch (error) {
+        console.error('Erro ao buscar usuário:', error);
+        res.redirect('/error');
+    }
+});
+
+// Rota para atualizar os dados do usuário
+app.post('/update-user/:id', async (req, res) => {
+    const { id } = req.params;
+    const { username, email, password } = req.body;
+
+    try {
+        await db.updateUser(id, username, email, password);
+        res.redirect('/admin');
+    } catch (error) {
+        console.error('Erro ao atualizar usuário:', error);
+        res.redirect('/error');
+    }
+});
+
+// Rota para excluir usuário
+app.delete("/user/:id", async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    try {
+        const userId = req.params.id;
+
+        // Obter desenhos associados ao usuário selecionado
+        const drawings = await db.getUserDrawings(userId);
+
+        // Excluir os desenhos associados ao usuário
+        for (const drawing of drawings) {
+            await db.deleteDrawing(drawing.id);
+        }
+
+        // Excluir o usuário após excluir os desenhos
+        const status = await db.deleteUser(userId);
+
+        res.json({
+            status
+        });
+    } catch (error) {
+        // Capturar e lidar com erros
+        console.error('Erro ao excluir usuário:', error);
+        res.status(500).json({
+            error: error.message
+        });
+    }
+});
+
+
+// Rota para excluir desenho
+app.delete('/drawing/:id', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    try {
+        const result = await db.deleteDrawing(req.params.id);
+        res.json({ status: result.affectedRows > 0 ? 'success' : 'error' });
+    } catch (error) {
+        console.error('Erro ao excluir desenho:', error);
+        res.json({ status: 'error' });
+    }
+});
 
 consign()
     .include("mvc/controllers")
@@ -165,6 +253,7 @@ app.use((err, req, res, next) => {
     res.status(500).render('ctrldev/error');
 });
 
-app.listen(3000, () => console.log("Online Server at port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 
 module.exports = app;
